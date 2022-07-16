4bab6072012066550c4a2957f4c3ddfcfa2de5f3
elbaz <eran.elbaz@env0.com>
2022-07-16 18:40:05 +0300

    Store word to db by document id (#2)

--- src/load-document.ts
+++ src/load-document.ts
@@ -1,8 +1,18 @@
 import * as csv from 'csv-string';
-import { saveSong } from './store';
+import { saveSong, saveWordsOfDocument } from './store';
+import _ from 'lodash';
 
 export const loadDocument = async (content: string) => {
   const parsedCsv = csv.parse(content, { output: 'objects' });
   console.log({ parsedCsv });
-  parsedCsv.map(row => saveSong(row as any, row['content']));
+  await Promise.all(
+    parsedCsv.map(async row => {
+      console.log({ row });
+      const songContent = row['content'];
+      const songId = await saveSong(row as any, songContent);
+
+      let words = _(songContent).split('\n').split(' ').flatten().value();
+      await saveWordsOfDocument(words, songId);
+    })
+  );
 };
