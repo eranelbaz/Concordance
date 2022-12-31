a6a34322856c4db98f4e5f33fef263f0c9c0d690
Elbaz <eran.elbaz@env0.com>
2022-12-31 14:22:48 +0200

    dump and load from files

---
+++ src/be/db/commands.sh
@@ -0,0 +1,4 @@
+# dump to files
+for n in $(mysq -u root -proot -p concordance -e "show tables" | grep -v Tables | tr -d '| '); do mysql -u root -proot -p concordance --xml -e "SELECT * FROM \`${n}\`" > ${n}.xml; done
+# load from files
+for n in $(mysql -u root -proot -p concordance -e "show tables" | grep -v Tables | tr -d '| '); do mysql --local_infile=1 -u root -proot -p concordance -e "LOAD XML LOCAL INFILE '${n}.xml' INTO TABLE \`${n}\`"; done
