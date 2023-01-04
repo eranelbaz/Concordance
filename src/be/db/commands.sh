63f90c853b89a6d9f5d9e87be1dacddd35b259bf
Elbaz <eran.elbaz@env0.com>
2023-01-04 18:03:23 +0200

    fixed commands

--- src/be/db/commands.sh
+++ src/be/db/commands.sh
@@ -1,6 +1,9 @@
 # into docker
 docker exec -it db_db_1  bash
 # dump to files
-for n in $(mysql -u root -proot -p concordance -e "show tables" | grep -v Tables | tr -d '| '); do mysql -u root -proot -p concordance --xml -e "SELECT * FROM \`${n}\`" > ${n}.xml; done
+
+for n in $(mysql -u root -proot -p concordance -e "show tables" | grep -v Tables | tr -d '| '); do mysqldump --xml -u root -proot -p concordance --tables ${n} > ${n}.xml; done
+
 # load from files
-for n in $(mysql -u root -proot -p concordance -e "show tables" | grep -v Tables | tr -d '| '); do mysql --local_infile=1 -u root -proot -p concordance -e "LOAD XML LOCAL INFILE '${n}.xml' INTO TABLE \`${n}\`"; done
+for n in $(mysql -u root -proot -p concordance -e "show tables" | grep -v Tables | tr -d '| '); do mysql --local_infile=1 --max-allowed-packet=2G -u root -proot -p concordance -e "LOAD XML CONCURRENT LOCAL INFILE '${n}.xml' INTO TABLE \`${n}\`"; done
+
