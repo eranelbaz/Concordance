1204875ae648b227438f0598052a9c1e38e5881a
Elbaz <eran.elbaz@env0.com>
2023-01-04 15:54:31 +0200

    update commands

--- src/be/db/commands.sh
+++ src/be/db/commands.sh
@@ -1,4 +1,6 @@
+# into docker
+docker exec -it db_db_1  bash
 # dump to files
-for n in $(mysq -u root -proot -p concordance -e "show tables" | grep -v Tables | tr -d '| '); do mysql -u root -proot -p concordance --xml -e "SELECT * FROM \`${n}\`" > ${n}.xml; done
+for n in $(mysql -u root -proot -p concordance -e "show tables" | grep -v Tables | tr -d '| '); do mysql -u root -proot -p concordance --xml -e "SELECT * FROM \`${n}\`" > ${n}.xml; done
 # load from files
 for n in $(mysql -u root -proot -p concordance -e "show tables" | grep -v Tables | tr -d '| '); do mysql --local_infile=1 -u root -proot -p concordance -e "LOAD XML LOCAL INFILE '${n}.xml' INTO TABLE \`${n}\`"; done
