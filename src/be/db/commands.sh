7c39d21e47e87ec82a665a0447cc94f521df53ef
Elbaz <eran.elbaz@env0.com>
2023-01-24 15:30:07 +0200

    extra load

--- src/be/db/commands.sh
+++ src/be/db/commands.sh
@@ -6,4 +6,5 @@ for n in $(mysql -u root -proot -p concordance -e "show tables" | grep -v Tables
 
 # load from files
 for n in $(mysql -u root -proot -p concordance -e "show tables" | grep -v Tables | tr -d '| '); do mysql --local_infile=1 --max-allowed-packet=2G -u root -proot -p concordance -e "LOAD XML CONCURRENT LOCAL INFILE '${n}.xml' INTO TABLE \`${n}\`"; done
-
+# Make sure to double load this one
+mysql --local_infile=1 --max-allowed-packet=2G -u root -proot -p concordance -e "LOAD XML CONCURRENT LOCAL INFILE 'groupsToWords.xml' INTO TABLE \`groupsToWords\`"
